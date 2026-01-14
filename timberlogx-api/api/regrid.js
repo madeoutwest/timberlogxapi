451b400714c306d66ef41796a445fe8b5f38bcc4
madeoutwest <madeoutwestllc@gmail.com>
2026-01-14 10:28:37 -0800

    Update regrid.js

--- timberlogx-api/api/regrid.js
+++ timberlogx-api/api/regrid.js
@@ -10,7 +10,8 @@ export default async function handler(req, res) {
   
   let url;
   if (endpoint === 'typeahead') {
-    url = `https://app.regrid.com/api/v1/typeahead.json?query=${encodeURIComponent(query)}&token=${REGRID_TOKEN}`;
+    // Add Oregon context to restrict results to Oregon state
+    url = `https://app.regrid.com/api/v1/typeahead.json?query=${encodeURIComponent(query)}&context=/us/or&token=${REGRID_TOKEN}`;
   } else if (endpoint === 'search') {
     url = `https://app.regrid.com/api/v1/search.json?query=${encodeURIComponent(query)}&context=${encodeURIComponent(context)}&limit=1&token=${REGRID_TOKEN}`;
   } else {
